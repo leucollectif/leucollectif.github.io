6bca545739a271c0be087bc858338c38548a6eab
jpirsch <jpirsch@student.42.fr>
2017-10-18 19:03:14 +0200

    fix hand display

--- fopWorld/player.js
+++ fopWorld/player.js
@@ -6,6 +6,7 @@ function Player(x, y, direction)
   this.decal = 0;
   this.height = 0;
   this.weapon = new Bitmap('assets/hand.png', 319, 320);
+  this.paces = 0;
 }
 
 Player.prototype.rotate = function(angle)
@@ -24,6 +25,7 @@ Player.prototype.walk = function(distance, map)
   var dy = Math.sin(this.direction) * distance;
   if (map.get(this.x + dx, this.y) <= 0) this.x += dx;
   if (map.get(this.x, this.y + dy) <= 0) this.y += dy;
+  this.paces += distance;
 };
 
 Player.prototype.update = function(controls, map, seconds)
