f7435c38dff683b34ca00c0b3fca7f93c26796c8
jpirsch <jeanpirsch@gmail.com>
2017-12-28 23:08:25 +0200

    Now loading obj with material and texture

--- srcs/load_OBJ.js
+++ srcs/load_OBJ.js
@@ -2,7 +2,7 @@ function load_OBJ(scene, texture, obj, position, scale) {
 	// texture
 	var manager = new THREE.LoadingManager();
 	manager.onProgress = function ( item, loaded, total ) {
-		console.log( item, loaded, total );
+		console.log( "item, loaded, total :", item, loaded, total );
 	};
 	var textureLoader = new THREE.TextureLoader( manager );
 	var texture = textureLoader.load( texture );
@@ -14,22 +14,39 @@ function load_OBJ(scene, texture, obj, position, scale) {
 			console.log( Math.round(percentComplete, 2) + '% downloaded' );
 		}
 	};
-	var onError = function ( xhr ) { };
+	var onError = function ( xhr ) {
+		console.log( "Error : ", xhr );
+	};
+	var mtlLoader = new THREE.MTLLoader( manager );
+	mtlLoader.setPath( 'models/Room-Door/' );
+	mtlLoader.load( 'Door_Component_BI3.mtl', function( materials ) {
+		materials.preload();
+		var objLoader = new THREE.OBJLoader( manager );
+		objLoader.setMaterials( materials );
+//		objLoader.setPath( 'obj/male02/' );
+		objLoader.load( obj, function ( object ) {
+			/*object.position.y = - 95;
+			scene.add( object );
+		}, onProgress, onError );
+	});
 
-	var ret;
 	var loader = new THREE.OBJLoader( manager );
-	loader.load( obj, function ( object ) {
+	loader.load( obj, function ( object ) {*/
 		object.traverse( function ( child ) {
-			if ( child instanceof THREE.Mesh )
+			if ( child instanceof THREE.Mesh ) {
+				console.log( "Child : ", child );
 				child.material.map = texture;
+			}
 		} );
+		console.log( "Object : ", object );
 		object.position.x = position.x;
 		object.position.y = position.y;
 		object.scale.x = scale.x;
 		object.scale.y = scale.y;
 		object.scale.z = scale.z;
-		ret = object;
-		scene.add( object )
+		scene.add( object );
 	}, onProgress, onError );
-	return ret;
+	console.log( "Loader : ", objLoader);
+	});
+//	console.log( "Loader : ", loader);
 }
--- srcs/map.js
+++ srcs/map.js
@@ -6,7 +6,7 @@ function Map( scene, camera, index ) {
 	scene.add( this.ambientLight );
 
 	if (index == 1) {
-	load_OBJ(scene, "img/UV_Grid_Sm.jpg", "models/fopMap2.obj",
+		load_OBJ(scene, "img/UV_Grid_Sm.jpg", "models/fopMap2.obj",
 			{x: 10, y: 1560}, {x: 100, y: 100, z: 100});
 /*
 		var spriteMaterial = new THREE.SpriteMaterial( { map: test_tex, color: 0xffffff } );
@@ -55,11 +55,21 @@ function Map( scene, camera, index ) {
 	}
 	else if (index == 0) {
 		var groundTex = new THREE.TextureLoader().load( 'img/ground_low.jpg' );
+		groundTex.wrapS = THREE.MirroredRepeatWrapping;
+		groundTex.wrapT = THREE.MirroredRepeatWrapping;
+		groundTex.repeat.set( 16, 16 );
 
-		var groundMaterial = new THREE.MeshBasicMaterial( { map: groundTex, color: 0xffffff } );
+		var groundMaterial = new THREE.MeshPhysicalMaterial( { map: groundTex, color: 0xffffff } );
 		var groundGeom = new THREE.CubeGeometry( 4000, 4, 4000, 1, 1, 1 );
 		var ground = new THREE.Mesh( groundGeom, groundMaterial );
 		scene.add( ground );
+
+		var sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xffffaa } );
+		var sphereGeom = new THREE.SphereGeometry( 40, 32, 32);
+		var sphere = new THREE.Mesh( sphereGeom, sphereMaterial );
+		sphere.translateOnAxis( { x:0, y:1, z:0 }, 1500);
+		sphere.translateOnAxis( { x:0, y:0, z:1 }, 250);
+		scene.add( sphere );
 
 		var listener = new THREE.AudioListener();
 		camera.add( listener );
@@ -72,8 +82,48 @@ function Map( scene, camera, index ) {
 			sound.play();
 		});
 		
-		var model = load_OBJ(scene, "", "models/door.obj",
+		load_OBJ(scene, 'models/Room-Door/textures/Door_C.jpg', "models/Room-Door/Door_Component_BI3.obj",
 				{x: 0, y: 0}, {x: 70, y: 70, z: 70});
 		ground.add( sound );
+/*
+// create a keyframe track (i.e. a timed sequence of keyframes) for each animated property
+				// Note: the keyframe track type should correspond to the type of the property being animated
+
+		var positionKF = new THREE.VectorKeyframeTrack( '.position', [ 0, 1, 2 ],
+														[ 0, 0, 0, 30, 0, 0, 0, 0, 0 ] );
+		var scaleKF = new THREE.VectorKeyframeTrack( '.scale', [ 0, 1, 2 ],
+													[ 1, 1, 1, 2, 2, 2, 1, 1, 1 ] );
+		// ROTATION
+		var xAxis = new THREE.Vector3( 1, 0, 0 );
+		var qInitial = new THREE.Quaternion().setFromAxisAngle( xAxis, 0 );
+		var qFinal = new THREE.Quaternion().setFromAxisAngle( xAxis, Math.PI );
+		var quaternionKF = new THREE.QuaternionKeyframeTrack( '.quaternion', [ 0, 1, 2 ],
+						[ qInitial.x, qInitial.y, qInitial.z, qInitial.w, qFinal.x, qFinal.y,
+						qFinal.z, qFinal.w, qInitial.x, qInitial.y, qInitial.z, qInitial.w ] );
+		// COLOR
+		var colorKF = new THREE.ColorKeyframeTrack( '.material.color', [ 0, 1, 2 ],
+							[ 1, 0, 0, 0, 1, 0, 0, 0, 1 ], THREE.InterpolateDiscrete );
+		// OPACITY
+		var opacityKF = new THREE.NumberKeyframeTrack( '.material.opacity', [ 0, 1, 2 ],
+														[ 1, 0, 1 ] );
+		// create an animation sequence with the tracks
+		// If a negative time value is passed, the duration will be calculated from the times of the passed tracks array
+		var clip = new THREE.AnimationClip( 'Action', 3,
+							[ scaleKF, positionKF, quaternionKF, colorKF, opacityKF ] );
+
+		this.mixer = new THREE.AnimationMixer( ground );
+		var clipAction = this.mixer.clipAction( clip );
+		clipAction.play();
+
+		this.clock = new THREE.Clock();
+*/
 	}
+}
+
+Map.prototype.update = function() {
+/*
+	var delta = this.clock.getDelta()
+	if (this.mixer)
+		this.mixer.update( delta );
+*/
 }
